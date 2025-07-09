9ac39ebd29d59f4f29a479449fba7f1a19426893
Silverplata <jplata@utedevas.es>
2025-07-09 23:42:57 +0200

    Backgorund gradient

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://porfolio.dev/',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        css: {
+          additionalData: `@import "src/styles/utils.css";`
+        }
+      }
+    }
+  }
 })
